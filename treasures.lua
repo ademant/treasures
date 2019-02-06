b7c20c32eb567e84d79a4aa0c664346bb039ee24
ademant <ademant@ademant.de>
2019-02-06 14:51:51 +0100

    changed initial fill of treasure nodes

--- treasures.lua
+++ treasures.lua
@@ -5,20 +5,13 @@ treasures.add_treasure("treasures:treasure1", {
 	drop = {
 		max_items = 5,
 		items = {
-			{items = {'minerdream:dagger 1'},rarity = 25,},
-			{items = {'minerdream:treasure2'},rarity = 15,},
+			{items = {'default:sword_steel 1'},rarity = 25,},
 			{items = {'default:wood 5'},rarity = 5,},
 			{items = {'default:stick 5'},rarity = 5,},
 			{items = {'default:torch 10'},rarity = 5,},
 			{items = {'default:apple 3'},rarity = 5,},
-			{items = {'minerdream:arrow 10'},rarity = 5,},
-			{items = {'minerdream:shuriken 15'},rarity = 5,},
 			{items = {'default:coal_lump 3'},rarity = 10,},
-			{items = {'minerdream:potassium_lump 3'},rarity = 10,},
-			{items = {'minerdream:calcium_lump 3'},rarity = 10,},
 			{items = {'default:flint 2'},rarity = 10,},
-			{items = {'minerdream:zinc_lump 3'},rarity = 15,},
-			{items = {'minerdream:nickel_lump 3'},rarity = 15,},
 			{items = {'default:iron_lump 3'},rarity = 15,},
 			{items = {'default:copper_lump 3'},rarity = 15,},
 			{items = {'default:tin_lump 3'},rarity = 15,},
@@ -33,24 +26,15 @@ treasures.add_treasure("treasures:treasure2", {
 	drop = {
 		max_items = 5,
 		items = {
-			{items = {'minerdream:shortsword 1'},rarity = 25,},
-			{items = {'minerdream:club 1'},rarity = 25,},
-			{items = {'minerdream:grenade 1'},rarity = 5,},
-			{items = {'minerdream:treasure3'},rarity = 25,},
-			{items = {'minerdream:shuriken 25'},rarity = 5,},
 			{items = {'default:tree 5'},rarity = 5,},
 			{items = {'default:wood 5'},rarity = 5,},
 			{items = {'default:torch 15'},rarity = 5,},
-			{items = {'minerdream:vitamin 3'},rarity = 5,},
-			{items = {'minerdream:potion_tiny 1'},rarity = 5,},
 			{items = {'default:coal_lump 5'},rarity = 10,},
 			{items = {'default:steel_ingot 3'},rarity = 10,},
 			{items = {'default:ladder_wood 15'},rarity = 5,},
-			{items = {'minerdream:lead_lump 3'},rarity = 15,},
 			{items = {'default:gold_lump 3'},rarity = 15,},
 			{items = {'default:mese_crystal 3'},rarity = 15,},
 			{items = {'default:diamond 2'},rarity = 20,},
-			{items = {'minerdream:arrow 15'},rarity = 5,},
 		}
 	},
 })
@@ -62,19 +46,11 @@ treasures.add_treasure("treasures:treasure3", {
 	drop = {
 		max_items = 5,
 		items = {
-			{items = {'minerdream:mace 1'},rarity = 25,},
-			{items = {'minerdream:treasure4'},rarity = 30,},
-			{items = {'minerdream:grenade 2'},rarity = 5,},
-			{items = {'minerdream:throwing_knife 15'},rarity = 5,},
 			{items = {'default:tree 5'},rarity = 5,},
 			{items = {'default:torch 25'},rarity = 5,},
-			{items = {'farming:bread 3'},rarity = 5,},
-			{items = {'minerdream:potion_tiny 2'},rarity = 3,},
 			{items = {'default:coal_lump 8'},rarity = 10,},
 			{items = {'default:steel_ingot 5'},rarity = 10,},
-			{items = {'minerdream:platinum_lump 3'},rarity = 15,},
 			{items = {'default:ladder_wood 20'},rarity = 5,},
-			{items = {'minerdream:arrow 20'},rarity = 5,},
 		}
 	},
 })
@@ -86,25 +62,10 @@ treasures.add_treasure("treasures:treasure4", {
 	drop = {
 		max_items = 5,
 		items = {
-			{items = {'minerdream:revolver'},rarity = 25,},
-			{items = {'minerdream:claymore'},rarity = 20,},
-			{items = {'minerdream:treasure5'},rarity = 20,},
-			{items = {'minerdream:throwing_knife 25'},rarity = 5,},
-			{items = {'minerdream:grenade 3'},rarity = 5,},
 			{items = {'default:torch 35'},rarity = 5,},
-			{items = {'farming:bread 5'},rarity = 5,},
-			{items = {'minerdream:potion_lesser 1'},rarity = 3,},
 			{items = {'default:coal_lump 10'},rarity = 10,},
-			{items = {'minerdream:bituminous_coal 5'},rarity = 15,},
 			{items = {'default:steel_ingot 8'},rarity = 10,},
-			{items = {'minerdream:cobalt_lump 3'},rarity = 15,},
-			{items = {'minerdream:ruthenium_lump 3'},rarity = 15,},
-			{items = {'minerdream:amethyst 1'},rarity = 15,},
-			{items = {'minerdream:garnet 1'},rarity = 15,},
-			{items = {'minerdream:jade 1'},rarity = 15,},
 			{items = {'default:ladder_wood 25'},rarity = 5,},
-			{items = {'minerdream:arrow 25'},rarity = 5,},
-			{items = {'minerdream:bullet 25'},rarity = 7,},
 		}
 	},
 })
@@ -116,25 +77,9 @@ treasures.add_treasure("treasures:treasure5", {
 	drop = {
 		max_items = 5,
 		items = {
-			{items = {'minerdream:shotgun'},rarity = 25,},
-			{items = {'minerdream:katana'},rarity = 20,},
-			{items = {'minerdream:treasure5'},rarity = 25,},
-			{items = {'minerdream:throwing_knife 30'},rarity = 5,},
-			{items = {'minerdream:grenade 4'},rarity = 5,},
 			{items = {'default:torch 50'},rarity = 5,},
-			{items = {'farming:bread 7'},rarity = 5,},
-			{items = {'minerdream:potion_lesser 2'},rarity = 3,},
-			{items = {'minerdream:bituminous_coal 8'},rarity = 10,},
 			{items = {'default:steel_ingot 10'},rarity = 10,},
-			{items = {'minerdream:rhodium_lump 3'},rarity = 15,},
-			{items = {'minerdream:tantalum_lump 3'},rarity = 15,},
-			{items = {'minerdream:onyx 1'},rarity = 15,},
-			{items = {'minerdream:spinel 1'},rarity = 15,},
-			{items = {'minerdream:topaz 1'},rarity = 15,},
-			{items = {'minerdream:aquamarine 1'},rarity = 15,},
 			{items = {'default:ladder_wood 30'},rarity = 5,},
-			{items = {'minerdream:arrow 30'},rarity = 5,},
-			{items = {'minerdream:bullet 30'},rarity = 7,},
 		}
 	},
 })
